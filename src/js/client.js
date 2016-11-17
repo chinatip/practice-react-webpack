4043a8506ec49acf736511ffc0a21434856e3a16
Chocolate <namtan8888@gmail.com>
2016-11-17 09:41:54 +0700

    Added file structure

---
+++ src/js/client.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Main extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>This is one cool app!</h1>
+      </div>
+    );
+  }
+}
